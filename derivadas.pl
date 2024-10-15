d860ace38bbf786b4fb34517e8192a50b1527d41
Hayser8 <105123207+Hayser8@users.noreply.github.com>
2024-10-15 15:11:04 -0600

    se agrega las funciones de tangente cotangente y arcotangente

--- derivadas.pl
+++ derivadas.pl
@@ -15,3 +15,16 @@ derivada(F * G, X, F * DG + G * DF) :-
 derivada(F / G, X, (DF * G - F * DG) / (G * G)) :- 
     derivada(F, X, DF), 
     derivada(G, X, DG).
+
+% Derivada de la función tangente
+derivada(tan(F), X, sec(F) * sec(F) * DF) :- 
+    derivada(F, X, DF).
+
+% Derivada de la función cotangente
+derivada(cot(F), X, -csc(F) * csc(F) * DF) :- 
+    derivada(F, X, DF).
+
+
+% Derivada de la función arcotangente
+derivada(atan(F), X, DF / (1 + F * F)) :-
+    derivada(F, X, DF).
