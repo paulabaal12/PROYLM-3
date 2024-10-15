5eaf825dc2612656e0dfb384e2d15879e8e5682f
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2024-10-15 17:13:41 -0600

    Update derivadas.pl

--- derivadas.pl
+++ derivadas.pl
@@ -3,10 +3,14 @@ derivada(C, _, 0) :- number(C).  % Derivada de constantes
 derivada(x, _, 1).  % Derivada de la variable x
 
 % Regla de la suma
-
+derivada(F + G, X, DF + DG) :- 
+    derivada(F, X, DF), 
+    derivada(G, X, DG).
 
 % Regla de la resta
-
+derivada(F - G, X, DF - DG) :- 
+    derivada(F, X, DF), 
+    derivada(G, X, DG).
 
 % Regla del producto
 derivada(F * G, X, F * DG + G * DF) :- 
@@ -23,15 +27,19 @@ derivada(exp(F), X, exp(F) * DF) :-
     derivada(F, X, DF).
 
 % Función logaritmo natural
-derivada(log(F), X, DF / F) :-
+derivada(ln(F), X, DF / F) :-
+    derivada(F, X, DF).
+
+% Función seno
+derivada(sen(F), X, cos(F) * DF) :-
+    derivada(F, X, DF).
+
+% Función coseno
+derivada(cos(F), X, -sen(F) * DF) :-
     derivada(F, X, DF).
 
 % Derivada de la función tangente
 derivada(tan(F), X, sec(F) * sec(F) * DF) :- 
-    derivada(F, X, DF).
-
-% Derivada de la función cotangente
-derivada(cot(F), X, -csc(F) * csc(F) * DF) :- 
     derivada(F, X, DF).
 
 % Derivada de la función arcotangente
@@ -41,5 +49,3 @@ derivada(atan(F), X, DF / (1 + F * F)) :-
 % Derivada de una potencia (F^N)
 derivada(F ** N, X, N * F ** (N - 1) * DF) :-
     derivada(F, X, DF).
-
-
